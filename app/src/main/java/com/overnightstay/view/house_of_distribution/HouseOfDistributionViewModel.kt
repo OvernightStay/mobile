f4daa47ef4001e34137399617a9c72640fea8aa0
Mea <maevski.ed@gmail.com>
2024-10-19 14:55:19 +0300

    Добавил xml view c пунктом выдачи.

---
+++ app/src/main/java/com/overnightstay/view/house_of_distribution/HouseOfDistributionViewModel.kt
@@ -0,0 +1,7 @@
+package com.overnightstay.view.house_of_distribution
+
+import androidx.lifecycle.ViewModel
+
+class HouseOfDistributionViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
